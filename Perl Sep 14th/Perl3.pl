c9efe57447ddc1c641e57ec937881188224cddf2
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-14 12:57:52 -0300

    some basic perl rogramms

---
+++ Perl Sep 14th/Perl3.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $name;
+print "What is your name? ";
+$name = <>;
+chomp $name;
+
+print "Hello $name! \n";
---
+++ Perl Sep 14th/Perl4.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+print "What is your name? ";
+my $name = <>;
+chomp $name;
+
+print "Hello $name! \n";
